c9ec81914dfb6a7b346afdb4459dfe2df923dcff
Crowdbotics <team@crowdbotics.com>
2024-05-24 19:29:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-recipe-48096",
+  url: "https://lucky-recipe-48096.botics.co",
+  api: "https://lucky-recipe-48096.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
